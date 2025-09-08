bd1a1f11b6487de05f296f590355730aafe4fbaf
t3edct-source <t3.edct@gmail.com>
2025-09-08 23:14:32 +0900

    fix: correct storageBucket

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,11 +7,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 // あなたのプロジェクト専用の firebaseConfig を貼る
 const firebaseConfig = {
   apiKey: "AIzaSyD7WHlIcOz1dwvp3nWOpvJdd9prSomZThA",
-  authDomain: "t3ed-af16f.firebaseapp.com",
-  projectId: "t3ed-af16f",
-  storageBucket: "t3ed-af16f.appspot.com",
-  messagingSenderId: "83126646265",
-  appId: "1:83126646265:web:116d169b511d76a4f7cbe8",
+  authDomain: "t3ed-a1f61.firebaseapp.com",
+  projectId: "t3ed-a1f61",
+  storageBucket: "t3ed-a1f61.firebasestorage.app",
+  messagingSenderId: "831266462265",
+  appId: "1:831266462265:web:116d109b511d76a4f7cbe8",
+  measurementId: "G-NM2EHERB79"
 };
 
 
